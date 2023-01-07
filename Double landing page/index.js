fe429335811657a4af354f21b78978ba4da70140
karthik242002 <karthiksiva153@gmail.com>
2023-01-07 13:32:22 +0530

    updated the landing

--- Double landing page/index.js
+++ Double landing page/index.js
@@ -6,11 +6,9 @@ const rightE1 = document.querySelector(".right")
 leftE1.addEventListener("mouseenter", ()=>{
     containerE1.classList.add("active-left")
 })
-
 leftE1.addEventListener("mouseleave", ()=>{
     containerE1.classList.remove("active-left")
 })
-
 rightE1.addEventListener("mouseenter", ()=>{
     containerE1.classList.add("active-right")
 })
