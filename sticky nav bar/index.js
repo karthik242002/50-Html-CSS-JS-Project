a75ce07c4c258ff26b01a908d798ebf3e5f64b33
karthik242002 <karthiksiva153@gmail.com>
2023-01-07 14:02:26 +0530

    updated the navbar

--- sticky nav bar/index.js
+++ sticky nav bar/index.js
@@ -1,13 +1,10 @@
 const navbarE1 = document.querySelector(".navbar");
-
 const bottomContainerE1 = document.querySelector(".bottom-container");
-
 console.log(navbarE1.offsetHeight);
 console.log(bottomContainerE1.offsetTop);
 window.addEventListener("scroll", ()=>{
     if(window.scrollY > bottomContainerE1.offsetTop - navbarE1.offsetHeight - 50){
         navbarE1.classList.add("active")
-
     }else{
         navbarE1.classList.remove("active")
     }
