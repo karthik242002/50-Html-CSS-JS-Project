1ed8dfb741e313cd20511f802e15a5e212e09817
karthik242002 <karthiksiva153@gmail.com>
2023-01-07 12:48:38 +0530

    updated the animated search bar

--- Animated search bar/index.js
+++ Animated search bar/index.js
@@ -1,5 +1,4 @@
 const searchBarContainerE1 = document.querySelector(".search-bar-container")
-
 const magnifierE1 = document.querySelector(".magnifier")
 
 magnifierE1.addEventListener("click", ()=>{
