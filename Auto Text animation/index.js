f827f3227e07aef41601700ea34b3510a36c01bf
karthik242002 <karthiksiva153@gmail.com>
2023-01-07 14:53:54 +0530

    updated the animation

---
+++ Auto Text animation/index.js
@@ -0,0 +1,27 @@
+const containerE1 = document.querySelector(".container");
+
+
+const careers = ["Student", "Programmer", "Web Developer", "Freelancer", "Instructor"];
+
+let careerIndex = 0
+
+
+let characterIndex = 0;
+updateText()
+
+function updateText(){
+    characterIndex++;
+containerE1.innerHTML = `
+<h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>
+`;
+
+if(characterIndex === careers[careerIndex].length){
+    careerIndex++
+    characterIndex = 0
+}
+
+if(careerIndex === careers.length){
+    careerIndex = 0;
+}
+setTimeout(updateText, 400)
+}
