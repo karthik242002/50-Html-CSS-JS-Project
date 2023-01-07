6e8470e05f81e5ecf8b5df946cedaa93a490cede
karthik242002 <karthiksiva153@gmail.com>
2023-01-07 13:43:11 +0530

    updated the animation

--- Heart trail animation/index.js
+++ Heart trail animation/index.js
@@ -9,11 +9,9 @@ bodyE1.addEventListener("mousemove", (event)=>{
     const size = Math.random()*100;
     spanE1.style.width = size + "px"
     spanE1.style.height = size + "px"
-
     bodyE1.appendChild(spanE1);
     setTimeout(()=>{
         spanE1.remove(); 
-
     }, 3000)
 
 })
