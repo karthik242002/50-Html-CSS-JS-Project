9c681bf203ceef8a03eb5614b14acb68dac4178a
karthik242002 <karthiksiva153@gmail.com>
2023-02-12 16:07:01 +0530

    updated the counter

--- Real-time Character counter/index.js
+++ Real-time Character counter/index.js
@@ -1,12 +1,12 @@
 const textareaE1 = document.getElementById("textarea");
 const totalCounterE1 = document.getElementById("total-counter");
 const remainingCounterE1 = document.getElementById("remaining-counter");
-textareaE1.addEventListener("keyup", ()=>{
-    updadateCounter();
-    
+textareaE1.addEventListener("keyup", () => {
+    updateCounter();
+
 });
-updadateCounter();
-function updadateCounter(){
+updateCounter();
+function updateCounter() {
     totalCounterE1.innerText = textareaE1.value.length;
     remainingCounterE1.innerText = textareaE1.getAttribute("maxLength") - textareaE1.value.length;
 }
